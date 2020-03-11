a98dd3bd095ce9ca3720a1ec6dcf12661b3040aa
chulkov <oleg.ponomarev@rasolution.ru>
2020-03-11 12:14:43 +0500

    first commit

---
+++ MemoryCardGame/Model/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  MemoryCardGame
+//
+//  Created by Max on 11/03/2020.
+//  Copyright © 2020 chulkov. All rights reserved.
+//
+
+import Foundation
---
+++ MemoryCardGame/Model/Game.swift
@@ -0,0 +1,9 @@
+//
+//  Game.swift
+//  MemoryCardGame
+//
+//  Created by Max on 11/03/2020.
+//  Copyright © 2020 chulkov. All rights reserved.
+//
+
+import Foundation
